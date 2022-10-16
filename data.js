1f5f2790125c9596e44c74e249a20dba9de93364
Akhil Sen Singh <115542173+heroicanchor@users.noreply.github.com>
2022-10-16 15:43:08 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,16 @@
+const people = [
+  {
+    img: "anas.png",
+    name: "Anas Parwez",
+    job: "Mechanical Engineer",
+    text: `Hi,I am Anas Parwez . Although being a ${"Mechanical Engineer".bold()} . <br>  I seek my interest in ${`Front-end Web development`.bold()} <br> and in ${`Competitive Coding.`.bold()} `,
+  },
+  {
+    img: "aryaman.jpeg",
+    name: "Aryaman Pandey",
+    job: "Mechanical Engineer",
+    text: `Hi,I am Aryaman Pandey . I am also a ${"Mechanical Engineer".bold()} . <br> I am in designing ${`of 3D and 2D components`.bold()} <br> and in ${`Front end Web development.`.bold()} `,
+  },
+];
+
+export default people;
